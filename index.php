aff381dff2430839c87ea9de36d9ec15a4b41343
yasirutomo <yasirutomo@gmail.com>
2021-05-18 11:48:19 +0800

    update ui

--- index.php
+++ index.php
@@ -1,94 +1,160 @@
-<html>
-    <head>
-        <title>
-            Live Editor untuk Belajar Web
-        </title>
-        <style>
-            * {
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+    <title>Live Editor untuk Belajar Web</title>
+    <meta name="viewport" content="width=device-width">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>
+        * {
+            -webkit-box-sizing: border-box;
+            -moz-box-sizing: border-box;
             box-sizing: border-box;
-            padding: 0;
-            margin: 0;
-            }
-
-            .btn {
-            poistion:fixed;
-            right:0;
-            padding: 0.4rem;
-            width: 4rem;
-            background: rgb(0, 0, 0);
-            color: gold;
-            font-size: 1rem;
-            outline:none;
-            cursor:pointer;
-            height:90vh;
-            }
-            .btn:hover{
-            color:white;
-            background: blue;
-            }
-
-            .main-editor {
-            background: rgba(0, 0, 0, 0.91);
-            display: flex;
-            width: 100%;
-            padding: 1rem;
-            box-shadow:0 2px 3px black;
-            position:fixed;
-            height:100vh;
-            justify-content: center;
-                align-items: center;
-            border: 7px solid #36383f;
-            }
-
-            .first {
+        }
+        body {
+            color:#000000;
+            margin:0px;
+            font-size:100%;
+        }
+        .trytopnav {
+            height:40px;
+            overflow:hidden;
+            min-width:380px;
+            position:absolute;
+            width:100%;
+            /* top:99px; */
+            background-color:#E7E9EB;
+        }
+        #framesize span {
+            xfont-family:Consolas, monospace;
+        }
+        #container {
+            background-color:#E7E9EB;
+            width:100%;
+            overflow:auto;
+            position:absolute;
+            top:48px;
+            bottom:0;
+            height:auto;
+        }
+        #textareacontainer, #iframecontainer {
+            float:left;
+            height:100%;
+            width:50%;
+        }
+        #textarea, #iframe {
+            height:100%;
+            width:100%;
+            padding-bottom:10px;
+            padding-top:1px;
+        }
+        #textarea {
+            padding-left:10px;
+            padding-right:5px;  
+        }
+        #iframe {
+            padding-left:5px;
+            padding-right:10px;  
+        }
+        #textareawrapper {
+            width:100%;
+            height:100%;
             background-color: #ffffff;
-            width: 50%;
-            overflow-x: hidden;
-            overflow-y: auto;
-            white-space: pre;
-            box-shadow: 0 1px 1px rgb(22, 22, 22);
-            outline: none;
-            padding: 0.4rem;
-            height: 90vh;
-            }
-
-            .second {
-            background-color: rgb(255, 255, 255);
-            width: 50%;
-            overflow-y: auto;
-            white-space: pre;
-            right: 0;
-            box-shadow: 0 1px 1px rgb(22, 22, 22);
-            padding: 0.4rem;
-                height: 90vh;
-            }
-        </style>
-    </head>
-</html>
+            position:relative;
+            box-shadow:0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24);
+        }
+        #iframewrapper {
+            width:100%;
+            height:100%;
+            -webkit-overflow-scrolling: touch;
+            background-color: #ffffff;
+            box-shadow:0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24);
+        }
+        #textareaCode {
+            background-color: #ffffff;
+            font-family: consolas,"courier new",monospace;
+            font-size:15px;
+            height:100%;
+            width:100%;
+            padding:8px;
+            resize: none;
+            border:none;
+            line-height:normal;    
+        }
+        #iframeResult, #iframeSource {
+            background-color: #ffffff;
+            height:100%;
+            width:100%;  
+        }
+        .trytopnav {
+            height:48px!important;
+        }
+    </style>
+</head>
 <body>
-    <div class="main-editor">
-        <button class="btn">Run</button>
-        <div class="first" contenteditable>
-            writecode
+    <div class="trytopnav">
+        <div style="overflow:auto">
+            <center><button style="margin:11px;">Run &raquo;</button></center>
         </div>
-        <iframe class="second"></iframe>
     </div>
 
+    <!-- halaman live kode -->
+    <div id="container">
+
+    <!-- S:bagian kiri (editor) -->
+    <div id="textareacontainer">
+        <div id="textarea">
+            <div id="textareawrapper">
+
+<!-- S:snippet code -->
+<textarea id="textareaCode" wrap="logical" spellcheck="false"><!DOCTYPE html>
+<html>
+<body>
+<h1>The input formenctype attribute</h1>
+
+<p>The formenctype attribute specifies how the form data should be encoded when submitted.</p>
+
+<form action="/tangkap.html" method="post">
+<label for="fname">First name:</label>
+<input type="text" id="fname" name="fname"><br><br>
+<input type="submit" value="Submit">
+<input type="submit" formenctype="multipart/form-data" value="Submit as Multipart/form-data">
+</form>
+
+</body>
+</html>
+</textarea>
+<!-- E:snippet code -->
+
+            </div>
+        </div>
+    </div>
+    <!-- E:bagian kiri (editor) -->
+
+        <!-- S:bagian kanan (hasilnya) -->
+    <div id="iframecontainer">
+        <div id="iframe">
+            <iframe id="iframewrapper"></iframe>
+        </div>
+    </div>
+    <!-- E:bagian kanan (hasilnya) -->
+
+    </div>
+    <!-- E:kontainer -->
+
     <script>
-    const first = document.querySelector(".first");
+    const first = document.getElementById("textareaCode");
     const iframe = document.querySelector("iframe");
     const btn = document.querySelector("button");
 
     btn.addEventListener("click", () => {
-    var html = first.textContent;
+    var html = first.value;
     iframe.src = "data:text/html;charset=utf-8," + encodeURI(html);
     });
 
-
-    first.addEventListener('keyup',()=>{
-    var html = first.textContent;
-    iframe.src = "data:text/html;charset=utf-8," + encodeURI(html);
-    })
+    // first.addEventListener('keyup',()=>{
+    // var html = first.value;
+    // iframe.src = "data:text/html;charset=utf-8," + encodeURI(html);
+    // })
 
     first.addEventListener("paste", function(e) {
         e.preventDefault();
@@ -96,4 +162,6 @@
         document.execCommand("insertText", false, text);
     });
     </script>
+
 </body>
+</html>
